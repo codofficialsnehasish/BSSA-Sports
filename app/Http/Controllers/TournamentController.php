f3ff1bf8f21ba52b524ac0b52bfb17309a8ac044
Snehasish Bhurisrestha <codofficial2023snehasish@gmail.com>
2024-10-28 13:47:12 +0530

    28-10-2024

--- app/Http/Controllers/TournamentController.php
+++ app/Http/Controllers/TournamentController.php
@@ -104,11 +104,18 @@ class TournamentController extends Controller
     }
 
     public function clubs(string $id){
-        $club_in_tournamets = ClubInTournamet::join('players_in_tournaments_clubs', 'club_in_tournamets.club_registrations_id', '=', 'players_in_tournaments_clubs.club_registrations_id')
-        ->select('club_in_tournamets.club_registrations_id', DB::raw('COUNT(players_in_tournaments_clubs.id) AS total_players'))
-        ->where('club_in_tournamets.tournaments_id', $id)
-        ->groupBy('club_in_tournamets.id', 'club_in_tournamets.club_registrations_id')
-        ->get();
+        // $club_in_tournamets = ClubInTournamet::join('players_in_tournaments_clubs', 'club_in_tournamets.club_registrations_id', '=', 'players_in_tournaments_clubs.club_registrations_id')
+        //         ->select('club_in_tournamets.club_registrations_id', DB::raw('COUNT(players_in_tournaments_clubs.id) AS total_players'))
+        //         ->where('club_in_tournamets.tournaments_id', $id)
+        //         ->groupBy('club_in_tournamets.id', 'club_in_tournamets.club_registrations_id')
+        //         ->get();
+
+        $club_in_tournamets = ClubInTournamet::leftJoin('players_in_tournaments_clubs', 'club_in_tournamets.club_registrations_id', '=', 'players_in_tournaments_clubs.club_registrations_id')
+                ->select('club_in_tournamets.club_registrations_id', DB::raw('COUNT(players_in_tournaments_clubs.id) AS total_players'))
+                ->where('club_in_tournamets.tournaments_id', $id)
+                ->groupBy('club_in_tournamets.id', 'club_in_tournamets.club_registrations_id')
+                ->get();
+
 
         // $club_in_tournamets = ClubInTournamet::where('tournaments_id',$id)
         //                                     ->withCount('players')
